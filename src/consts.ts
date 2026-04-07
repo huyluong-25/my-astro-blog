29c144e0e9fb04513ac3909e80804deb8c6e1f68
Huy <luongvanhuy25102004@gmail.com>
2026-04-07 10:17:01 +0700

    Cap nhat giao dien blog template

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'HuyHandsome';
+export const SITE_DESCRIPTION = 'Nơi chia sẻ kiến thức hệ thống, network và chuyện nghề IT';
