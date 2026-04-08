e19d8fc7a7275391886a0b153c4323d841f31f77
Huy <luongvanhuy25102004@gmail.com>
2026-04-08 16:20:45 +0700

    Static build (no adapter) - Cloudflare auto deploys functions

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,10 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
-import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
-	output: 'server',
-	adapter: cloudflare(),
+	output: 'static',
 	integrations: [mdx(), sitemap()],
 });
