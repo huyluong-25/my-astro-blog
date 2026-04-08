e1b6d349465cd272d4b4efec219968c66bb94e8b
Huy <luongvanhuy25102004@gmail.com>
2026-04-08 16:14:10 +0700

    Remove Cloudflare adapter for static build

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,11 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
-import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
 	output: 'static',
-	adapter: cloudflare({
-		imageService: 'compile'
-	}),
 	integrations: [mdx(), sitemap()],
 	image: {
 		domains: ['images.unsplash.com'],
